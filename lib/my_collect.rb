b64b1c50e0fa14eb846fd4244ab4a89c5eb0d2e2
Megan Collier <meganraec@live.com>
2018-03-12 04:33:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = Array.new
   while i < array.length
-    collection << yeild(array[i])
+    collection << yield(array[i])
     i += 1
   end
   collection
